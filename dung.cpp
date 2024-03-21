65e9b3da1995963d6bdaad53180a9a7a28d54c7a
dung11205 <vandunght00000@gamil.com>
2024-03-21 14:33:33 +0700

    hdt

---
+++ dung.cpp
@@ -0,0 +1,76 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "Every age has a language of its own\n";
+//     return 0;
+   
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "Nguyễn Văn Dũng" <<endl;
+//     cout << "18" <<endl;
+//     cout << "11/11/2005" <<endl;
+//     cout << "nam" <<endl;
+//     cout << "Lớp co23A" <<endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std; 
+// int main()
+// {
+//     int a = 7;
+//     int b = 2;
+//     int c = a + b;
+//     cout <<"tong 2 so la:" <<c;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+// int var1; 
+// int var2; 
+// var1 = 20; 
+// var2 = var1 + 10;
+// cout << "var1+10 is "; 
+// cout << var2 << endl; 
+// return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     char V1 = 68, V2 = 85, V3 = 78, V4 = 71;
+//     cout << V1 << V2 << V3 << V4 << endl;
+// }
+
+// #include <iostream> //for cout, etc.
+// using namespace std;
+// int main()
+// {
+// float rad; //variable of type float
+// const float PI = 3.14159F;
+// //type const float
+// cout << "Enter radius of circle: "; //prompt
+// cin >> rad; //get radius
+// float area = PI * rad * rad; //find area
+// cout << "Area is " << area << endl;
+// //display answer
+// return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+bool x = true;
+bool y = false;
+if(x) cout << "X is true";
+else cout << "X is false";
+}   
